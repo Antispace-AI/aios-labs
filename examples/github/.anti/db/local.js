0b24b0e3956ac6f83a5f0529db73157f6d0758e4
chingun <chingun@umich.edu>
2025-04-22 08:57:55 -0700

    done

--- examples/github/.anti/db/local.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const ioredis_1 = require("ioredis");
-const db = new ioredis_1.Redis(process.env.CACHE_REDIS_URL || "redis://localhost:6379");
-exports.default = db;
